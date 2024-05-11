801e11911cb2e57f91bd751e8c7b149fc9571a02
Jedabuka <pgsun@yandex.ru>
2024-05-12 00:45:57 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,7 @@
+my_string = input("Напишите пожалуйста предложение с пробелами: ")
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
