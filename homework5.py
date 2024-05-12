d679eca97633a5aac5ef6c78983c2d5bf9e56bd4
Jedabuka <pgsun@yandex.ru>
2024-05-12 22:44:07 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['avocado', 'apple', 'durian', 'banana', 'mango']
+print(my_list)
+print(my_list[0:5:4])
+print(my_list[2:4])
+my_list[2] = 'papaya'
+print(my_list)
+
+my_dict = {'chair': 'стул', 'pen': 'ручка', 'short': 'короткий', 'long': 'длинный'}
+print(my_dict)
+print(my_dict['short'])
+my_dict['tea'] = 'чай'
+print(my_dict)
