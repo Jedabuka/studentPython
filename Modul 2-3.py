18d9ca46acca4e995bcd216c2f43d4ae54f6fc3c
Jedabuka <pgsun@yandex.ru>
2024-05-18 13:32:43 +0300

    Add files via upload

--- Modul 2-3.py
+++ Modul 2-3.py
@@ -1,13 +1,6 @@
 car_list = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
 
-for i in car_list:
-    print('Я езжу на автомобиле марки', i)
-    
-    cars_count = 0
-
-    for i in range(len(car_list[1:])):
-        cars_count += 10
-
-print(cars_count)
-
-
+for i in range(0, 5):
+    cars_count += 10
+    print('Я езжу на автомобиле марки', car_list[i], cars_count)
