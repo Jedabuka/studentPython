f91e93a3b378a0928f5e9b8457cabf3bd2e3f78f
Jedabuka <pgsun@yandex.ru>
2024-05-21 20:40:52 +0300

    Add files via upload

---
+++ Modul 2-3.py
@@ -0,0 +1,11 @@
+car_list = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+
+for i in range(0, 5):
+    step = i
+    if i >= 1:
+        cars_count += 10
+    else:
+        i <= 1
+        cars_count = 0
+    print('Я езжу на автомобиле марки', car_list[i], cars_count)
