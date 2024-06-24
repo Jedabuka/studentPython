f9f3f203f5171c88d514e37a7ab862f59ab3e326
Jedabuka <pgsun@yandex.ru>
2024-06-24 17:54:18 +0500

    Add files via upload

---
+++ Module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as fm
+from true_math import divide as tm
+
+result1 = fm(33,11)
+print(result1)
+result2 = fm(30,0)
+print(result2)
+result3 = tm(44, 11)
+print(result3)
+result4 = tm(40,0)
+print(result4)
---
+++ fake_math.py
@@ -0,0 +1,7 @@
+def divide(first, second):
+    if second == 0:
+        return 'Ошибка'
+
+    else:
+        answer = first / second
+        return answer
