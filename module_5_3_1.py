f067289de792231edd5023381965683851c08af3
Jedabuka <pgsun@yandex.ru>
2024-07-13 15:35:36 +0300

    Update module_5_3_1.py

--- module_5_3_1.py
+++ module_5_3_1.py
@@ -3,7 +3,7 @@ class House:
     houses_history = []
 
     def __new__(cls, *args, **kwargs):
-        House.houses_history.append(args[0])
+        cls.houses_history.append(args[0])
         return super().__new__(cls)
 
     def __init__(self, name, number_of_floors):
