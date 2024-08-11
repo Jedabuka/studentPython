f36fe77b1b9c723133692b3f9819ec6762a1b02f
Jedabuka <pgsun@yandex.ru>
2024-08-11 20:02:47 +0300

    Add files via upload

--- module_7_1.py
+++ module_7_1.py
@@ -16,10 +16,29 @@ class Shop:
 
     def get_products(self):
         file = open(self.__file_name, 'r')
-        pprint(file.read())
-        file.close()
+        return file.read()
 
     def add(self, *products):
+        for product in products:
+            if product.name not in self.get_products():
+                file = open(self.__file_name, 'a')
+                file.write(str(product))
+                file.write('\n')
+                file.close()
+
+            else:
+                print(f'Продукт {product.name} уже есть в магазине')
+
 
 
 s1 = Shop()
+
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2)
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
