21c8413dc89447f41cd19072182c5ef323ad030a
Jedabuka <pgsun@yandex.ru>
2024-08-11 16:32:22 +0300

    Add files via upload

---
+++ module_7_1.py
@@ -0,0 +1,25 @@
+from pprint import pprint
+
+
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+
+class Shop:
+    __file_name = 'products.txt'
+
+    def get_products(self):
+        file = open(self.__file_name, 'r')
+        pprint(file.read())
+        file.close()
+
+    def add(self, *products):
+
+
+s1 = Shop()
