a716724551e70b53d48db0f677b045a5c7d501d7
Jedabuka <pgsun@yandex.ru>
2024-10-04 22:42:43 +0300

    Add files via upload

--- module_11_2.py
+++ module_11_2.py
@@ -2,29 +2,25 @@ import inspect
 import sys
 
 
-class MyClass:
-    def __init__(self, obj):
-        self.obj = obj
-        self.introspection_info()
+def introspection_info(obj):
+    attributes = []
+    methods = []
 
-    def introspection_info(self):
-        intro_dict = {}
-        attributes = []
-        methods = []
-        intro_dict.update({'type': type(self.obj).__name__})
-        for attr in dir(self.obj):
-            if callable(getattr(self.obj, attr)):
-                methods.append(attr)
-                intro_dict.update({'methods': methods})
-            else:
-                attributes.append(attr)
-                intro_dict.update({'attributes': attributes})
-        intro_dict.update({'module': inspect.getmodule(MyClass)})
-        intro_dict.update({'builtin': inspect.isbuiltin(self.obj)})
-        intro_dict.update({'id': id(self.obj)})
-        intro_dict.update({'size': sys.getsizeof(self.obj)})
+    for attr in dir(obj):
+        if callable(getattr(obj, attr)):
+            methods.append(attr)
+        else:
+            attributes.append(attr)
 
-        return print(intro_dict)
+    return {'type': type(obj).__name__,
+            'attributes': attributes,
+            'methods': methods,
+            'module': inspect.getmodule(obj),
+            'builtin': inspect.isbuiltin(obj),
+            'id': id(obj),
+            'size': sys.getsizeof(obj)
+            }
 
 
-my_class = MyClass(42)
+number_info = introspection_info(42)
+print(number_info)
