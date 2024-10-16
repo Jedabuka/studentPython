ad738044dd0b0903c94f7f09758de8606429dda1
Jedabuka <pgsun@yandex.ru>
2024-10-16 22:24:59 +0300

    Add files via upload

--- module_14_1.py
+++ module_14_1.py
@@ -1,31 +1,36 @@
 import sqlite3
 
 
-with sqlite3.connect('not_telegram.db') as conn:
-    cursor = conn.cursor()
+conn = sqlite3.connect('not_telegram.db')
+cursor = conn.cursor()
 
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Users(
-    id INTEGER PRIMARY KEY,
-    username TEXT NOT NULL,
-    email TEXT NOT NULL,
-    age INTEGER,
-    balance INTEGER NOT NULL
-    )
-    ''')
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL UNIQUE,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
 
-# for i in range(1, 11):
-#     cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
-#                    (f'User{i}', f'example{i}@gmail.com', f'{i * 10}', '1000'))
+cursor.execute('CREATE INDEX IF NOT EXISTS idx_email ON Users (email)')
 
-# for i in range(1, 11, 2):
-#     cursor.execute(f'UPDATE Users SET balance = ? WHERE username = ?', ('500', f'User{i}'))
+for i in range(1, 11):
+    cursor.execute('INSERT OR IGNORE INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+                    (f'User{i}', f'example{i}@gmail.com', f'{i * 10}', '1000'))
 
-# for i in range(1, 11, 3):
-#     cursor.execute('DELETE FROM Users WHERE username = ?', (f'User{i}',))
+for i in range(1, 11, 2):
+    cursor.execute(f'UPDATE Users SET balance = ? WHERE username = ?', ('500', f'User{i}'))
+
+for i in range(1, 11, 3):
+    cursor.execute('DELETE FROM Users WHERE username = ?', (f'User{i}',))
 
 cursor.execute(f'SELECT username, email, age, balance FROM Users WHERE age != ?', (60,))
 users = cursor.fetchall()
 
 for user in users:
     print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]}, Баланс: {user[3]}')
+
+conn.commit()
+conn.close()
