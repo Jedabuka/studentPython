e63a687367adde622f93c360f22fbe566bd1b261
Jedabuka <pgsun@yandex.ru>
2024-11-08 14:59:11 +0300

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -11,18 +11,25 @@ async def all_users() -> dict:
     return users_db
 
 @app.post('/user/{username}/{age}')
-async def new_user(username: str, age: int) -> str:
+async def new_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username',
+                                                 example='UrbanUser')]
+                   , age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='20')]) -> str:
     current_index = str(int(max(users_db, key=int)) + 1)
     users_db[current_index] = username, age
     return f'User {current_index} is registered'
 
+
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: str, username: str, age: int) -> str:
+async def update_user(user_id: Annotated[str, Path(ge=1, le=200, description='Enter User ID', example=111)],
+                      username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username',
+                                                    example='UrbanUser')],
+                      age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='20')]) -> str:
     users_db[user_id] = username, age
     return f'User {user_id} has been updated'
 
+
 @app.delete('/user/{user_id}')
-async def user_del(user_id: str) -> str:
+async def user_del(user_id: Annotated[str, Path(ge=1, le=200, description='Enter User ID', example=111)]) -> str:
     users_db.pop(user_id)
     return f'User {user_id} has been deleted'
 
