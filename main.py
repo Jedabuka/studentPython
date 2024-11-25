5521d37c3bde4e70c560ec8a5f6de460486e35a5
Jedabuka <pgsun@yandex.ru>
2024-11-25 11:49:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# 1st program
+a = (9 ** 0.5) * 5
+print(a)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+# 4th program
+a = '123.456'
+b = '4'
+if b in a:
+    print(b)
+# ---------------
+print(a[4])
+# ----------------
+c = float(a) * 10
+print(int(c) % 10)
